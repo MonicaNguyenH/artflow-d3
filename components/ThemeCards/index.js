99cd743b8f1105c9b12e09d93100bbc12a1c5d7c
shirinf <sforouzan@my.bcit.ca>
2024-03-25 12:12:27 -0700

    finished themecard and usercard components

--- components/ThemeCards/index.js
+++ components/ThemeCards/index.js
@@ -1,14 +1,21 @@
 import styles from './ThemeCards.module.css'
 
 export default function ThemeCards() {
-
   return (
     <>
         <div className={styles.parentContainer}>
-            <div className={styles.card1}> </div>
-            <div className={styles.card2}> </div>
-            <div className={styles.card3}> </div>
-            <div className={styles.card4}> </div>
+            <div className={styles.card1}>
+                <p className={styles.cardTitle}>Challenge</p>
+            </div>
+            <div className={styles.card2}>
+                <p className={styles.cardTitle}>Challenge</p>
+            </div>
+            <div className={styles.card3}>
+                <p className={styles.cardTitle}>Challenge</p>
+            </div>
+            <div className={styles.card4}>
+                <p className={styles.cardTitle}>Challenge</p>
+            </div>
         </div>
     </>
   )
