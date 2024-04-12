01d977d3ea7e290e1964b0483a8b521d12ee74b8
shirinf <sforouzan@my.bcit.ca>
2024-04-11 19:24:51 -0700

    landing page and signin/signup pages created and styled

--- components/Buttons/index.js
+++ components/Buttons/index.js
@@ -1,31 +1,47 @@
 import styles from './Buttons.module.css';
 
-export default function Buttons() {
-    return(
-        <>
+export function SigninButton() {
+    return (
         <div className={styles.gradientContainer}>
             <button className={styles.gradientButton}>Sign In</button>
         </div>
+    );
+}
 
+export function SignupButton() {
+    return (
         <div className={styles.gradientContainer}>
             <button className={styles.gradientButton}>Sign Up</button>
         </div>
+    );
+}
+
+export function ContinueButton() {
+    return (
         <div className={styles.ContinueButton}>
             <button className={styles.gradientContinueButton}>Continue</button>
         </div>
+    );
+}
+
+export function GoogleSigninButton() {
+    return (
         <div className={styles.socialContainer}>
             <button className={styles.socialButton}>
-                <img  className={styles.socialIcon} src="/images/google-icon.png" alt="Google Logo" width="25" height="25"/>
+                <img className={styles.googleIcon} src="/images/google-icon.png" alt="Google Logo" width="50" height="50"/>
                 Sign In with Google
             </button>
         </div>
+    );
+}
+
+export function AppleSigninButton() {
+    return (
         <div className={styles.socialContainer}>
             <button className={styles.socialButton}>
-                <img className={styles.socialIcon} src="/images/apple-icon.png" alt="Apple Logo" width="25" height="25"/>
+                <img className={styles.appleIcon} src="/images/apple-icon.png" alt="Apple Logo" width="50" height="50"/>
                 Sign In with Apple
             </button>
         </div>
-        </>
-    )
-    
+    );
 }
