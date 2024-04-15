15f2472b01850d0d23305a88048db471be029484
Callista <callyjchan@gmail.com>
2024-04-14 17:53:58 -0700

    homepage

--- pages/Homepage/index.js
+++ pages/Homepage/index.js
@@ -1,13 +1,20 @@
 import ThemeCards from "@/components/ThemeCards"
 import NavBar from "@/components/NavBar"
 import PromptDesc from "@/components/PromptDesc"
+import UserCard from "@/components/UserCard"
 import styles from "/styles/Homepage.module.css"
 
 export default function Homepage() {
     return (
         <>
+        <div className={styles.user}>
+            <UserCard />
+        </div>
        <div className={styles.cards}>
         <ThemeCards />
+        </div>
+        <div className={styles.prompt}>
+            <PromptDesc />
         </div>
         <div className={styles.bar}>
         <NavBar />
