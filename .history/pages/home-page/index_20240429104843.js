a7f3b6ba9f72625acdee576d154162ae21a7acaf
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2024-04-29 10:51:33 -0700

    conflicts resolved

---
+++ .history/pages/home-page/index_20240429104843.js
@@ -0,0 +1,31 @@
+import ThemeCards from "@/components/ThemeCards"
+import NavBar from "@/components/NavBar"
+import PromptDesc from "@/components/PromptDesc"
+import UserCard from "@/components/UserCard"
+import styles from "/styles/Homepage.module.css"
+import ComProject from "@/components/ComProject"
+import FeaturedPrompt from "@/components/FeaturedPrompt"
+import HeaderArea from "@/components/HeadArea"
+
+export default function Homepage() {
+    return (
+        <>
+            <div className={styles.main}>
+                <HeaderArea title="Home Page" description="Home page"/>
+                <div className={styles.user}>
+                    <UserCard />
+                </div>
+                <div className={styles.cards}>
+                    <ThemeCards />
+                </div>
+                <div className={styles.prompt}>
+                    <FeaturedPrompt />
+                </div>
+                <div className={styles.bar}>
+                    <NavBar />
+                </div>
+            </div>
+       </>
+    )
+}
+
