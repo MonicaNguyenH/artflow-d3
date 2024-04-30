e6e6e61ee4f9c11b9a0b9fa11c807e80dcc108b1
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2024-04-30 01:49:30 -0700

    Profile page ver1

---
+++ .history/components/ProfileTab/index_20240430010752.js
@@ -0,0 +1,12 @@
+import styles from './ProfileTab.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+export default function ProfileTab() {
+  return (
+    <>
+        
+    </>
+  )
+}
---
+++ .history/components/ProfileTab/index_20240430012011.js
@@ -0,0 +1,47 @@
+import styles from './ProfileTab.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import React, { useState } from 'react';
+
+export default function ProfileTab() {
+    const [activeTab, setActiveTab] = useState(1);
+    const handleTabChange = (tabIndex) => {
+        setActiveTab(tabIndex);
+    };
+
+    return (
+    <>
+        <div>
+            <div className="tab">
+                <button
+                    className={activeTab === 1 ? 'active' : ''}
+                    onClick={() => handleTabChange(1)}>
+                    <img className={styles.avatar} src="/images/grid-profile.svg" alt="dog profile image"/>
+                </button>
+                <button
+                    className={activeTab === 2 ? 'active' : ''}
+                    onClick={() => handleTabChange(2)}>
+                    <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                </button>
+            </div>
+            <div className="tab-content">
+                {activeTab === 1 && 
+                    <div>
+                        <img className={styles.avatar} src="/images/profile/profile-img-1.png" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                        <img className={styles.avatar} src="/images/heart-white-profile.svg" alt="dog profile image"/>
+                    </div>
+                }
+                {activeTab === 2 && <p>Content of Tab 2</p>}
+            </div>
+        </div>
+    </>
+    )
+}
