2a75b79b432756fa71fb51140f688a8ed7199598
MonicaNguyenH <144722804+MonicaNguyenH@users.noreply.github.com>
2024-05-06 15:55:38 -0700

    fix img on celestial-symphony_Object-fit: cover;

--- .history/pages/celestial-symphony/index_20240506155441.js
+++ .history/pages/celestial-symphony/index_20240506155441.js
@@ -19,19 +19,19 @@ export default function celestialSymphony() {
         </div>
         <div className={styles.container}>
             <img src="/images/image1.jpg" width={120} height={120}/>
-            <img src="/images/image2.jpg" width={120} height={120}/>
-            <img src="/images/image3.jpg" width={120} height={120}/>
-            <img src="/images/image4.jpg" width={120} height={120}/>
-            <img src="/images/image5.jpg" width={120} height={120}/>
-            <img src="/images/image6.jpg" width={120} height={120}/>
-            <img src="/images/image7.jpg" width={120} height={120}/>
-            <img src="/images/image8.jpg" width={120} height={120}/>
-            <img src="/images/image9.jpg" width={120} height={120}/>
-            <img src="/images/image10.jpg" width={120} height={120}/>
-            <img src="/images/image11.jpg" width={120} height={120}/>
-            <img src="/images/image12.jpg" width={120} height={120}/>
-            <img src="/images/image13.jpg" width={120} height={120}/>
-            <img src="/images/image14.jpg" width={120} height={120}/>
+            <img src="/images/image2.jpg" width={110} height={110}/>
+            <img src="/images/image3.jpg" width={110} height={110}/>
+            <img src="/images/image4.jpg" width={110} height={110}/>
+            <img src="/images/image5.jpg" width={110} height={110}/>
+            <img src="/images/image6.jpg" width={110} height={110}/>
+            <img src="/images/image7.jpg" width={110} height={110}/>
+            <img src="/images/image8.jpg" width={110} height={110}/>
+            <img src="/images/image9.jpg" width={110} height={110}/>
+            <img src="/images/image10.jpg" width={110} height={110}/>
+            <img src="/images/image11.jpg" width={110} height={110}/>
+            <img src="/images/image12.jpg" width={110} height={110}/>
+            <img src="/images/image13.jpg" width={110} height={110}/>
+            <img src="/images/image14.jpg" width={110} height={110}/>
         </div>
         <div className={styles.footer}>
             <NavBar/>
