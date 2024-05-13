5b7b80cbb10862e64facc3e7f193962413dc8931
shirinf <sforouzan@my.bcit.ca>
2024-05-12 20:02:23 -0700

    hid scrollbar on submit page, tweaked bio on our team page

--- pages/our-team/index.js
+++ pages/our-team/index.js
@@ -21,7 +21,7 @@ export default function celestialSymphony() {
                 <div className={styles.profileContent}>
                     <h1>Shirin Forouzan</h1>
                     <h2>Designer/Developer</h2>
-                    <p>As an art history lover and casual artist/designer, Shirin came up with the Artflow concept to inspire artists to practice daily to beat their creativity block. Shirin also loves to play video games and play guitar in her free time.</p>
+                    <p>As an art history lover and designer, Shirin came up with the Artflow concept to inspire artists to practice daily & beat their creative block. Shirin also loves to play video games and play guitar in her free time.</p>
                 </div>
             </div>
             <div className={styles.profile}>
@@ -31,7 +31,7 @@ export default function celestialSymphony() {
                 <div className={styles.profileContent}>
                     <h1>Monica Nguyen</h1>
                     <h2>Designer/Developer</h2>
-                    <p>As a tired designer and developer with a passion for good grades, Monica developed the ArtFlow app for creators to use art as a medium to relax and combat art block. Likes to eat, hate life sometimes.</p>
+                    <p>As a tired designer and developer with a passion for good grades, Monica developed the ArtFlow app for creators to use art as a medium to relax and combat art block. Likes to eat, hates life sometimes.</p>
                 </div>
             </div>
             <div className={styles.profile}>
